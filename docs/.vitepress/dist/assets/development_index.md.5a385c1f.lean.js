0e45e1a9929070a0e929f0e788060ee130166d15
Lais Santos <lais.santos@axiadigitalsolutions.com>
2023-05-23 16:58:16 -0300

    Fixing page not found issue

---
+++ docs/.vitepress/dist/assets/development_index.md.5a385c1f.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o,a as n}from"./app.aafb5b6d.js";const u=JSON.parse('{"title":"🖥️ Development","description":"","frontmatter":{},"headers":[],"relativePath":"development/index.md"}'),i={name:"development/index.md"},r=n("",10),a=[r];function s(l,p,d,c,h,m){return o(),t("div",null,a)}const v=e(i,[["render",s]]);export{u as __pageData,v as default};
